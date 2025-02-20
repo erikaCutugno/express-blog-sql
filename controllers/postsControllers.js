2ef0b6b844ffc0ee2fd79dd77109445ec13597d9
erikaCutugno <erikucciapop@gmail.com>
2025-02-20 22:43:47 +0100

    feat: add milestone 4 show

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -19,17 +19,15 @@ const index = (req, res) => {
 //Show
 const show = (req, res) => {
   const id = parseInt(req.params.id);
-  const posts = postsData.find((elm) => elm.id === id);
-  //   if (!posts) {
-  //     res.status(404);
 
-  //     return res.json({
-  //       status: 404,
-  //       error: "Not Found",
-  //       message: "Post non trovato",
-  //     });
-  //   }
-  res.json(posts);
+  const sql = "SELECT * FROM posts WHERE id = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (results.length === 0)
+      return res.status(404).json({ error: "Pizza not found" });
+    res.json(results[0]);
+  });
 };
 
 //Store
