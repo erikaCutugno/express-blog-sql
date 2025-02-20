debc4ff36dd6dade2fe1ed4284e94454751e44be
erikaCutugno <erikucciapop@gmail.com>
2025-02-20 22:37:51 +0100

    feat: add destroy

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -80,20 +80,17 @@ const modify = (req, res) => {
 //Destroy
 const destroy = (req, res) => {
   const id = parseInt(req.params.id);
-  const posts = postsData.find((elm) => elm.id === id);
 
-  //   if (!posts) {
-  //     res.status(404);
+  const sql = "DELETE FROM posts WHERE id = ?";
 
-  //     return res.json({
-  //       status: 404,
-  //       error: "Not Found",
-  //       message: "Post non trovato",
-  //     });
-  //   }
-  postsData.splice(postsData.indexOf(posts), 1);
-  console.log(postsData);
-  res.sendStatus(204);
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Failed to delete post",
+      });
+    }
+    res.sendStatus(204);
+  });
 };
 
 module.exports = { index, show, store, update, modify, destroy };
